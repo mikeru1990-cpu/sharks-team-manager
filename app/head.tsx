42e751e7e35e746a45d92305f611297786bc2790
mikeru1990-cpu <mikeru1990@googlemail.com>
2026-04-18 17:26:39 +0100

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="Sharks Team Manager" />
+      <link rel="apple-touch-icon" href="/icon-192.png" />
+    </>
+  )
+}
