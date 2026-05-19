fc08a6ce95521a96101c511498f0ba1cc1acb424
mikeru1990-cpu <mikeru1990@googlemail.com>
2026-05-19 15:31:33 +0100

    Add PlayersScreen component

---
+++ app/components/players/PlayersScreen.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function PlayersScreen() {
+  return (
+    <div style={{ paddingBottom: 120 }}>
+      <h1>Players</h1>
+      <p>Operational squad management</p>
+    </div>
+  )
+}
