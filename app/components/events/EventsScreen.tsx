498342dd39a4068971b9dab40b6c2f9cca142146
mikeru1990-cpu <mikeru1990@googlemail.com>
2026-05-19 15:31:57 +0100

    Add EventsScreen component

---
+++ app/components/events/EventsScreen.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function EventsScreen() {
+  return (
+    <div style={{ paddingBottom: 120 }}>
+      <h1>Events</h1>
+      <p>Training, fixtures and recurring schedules</p>
+    </div>
+  )
+}
